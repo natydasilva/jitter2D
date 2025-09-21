2dec32d6748c46c2d7e53252d399e60d1f554020
nachalca <nachalca@gmail.com>
2025-09-21 21:35:41 +1000

    stuff

--- tests/sandbox/stuff.R
+++ tests/sandbox/stuff.R
@@ -2,5 +2,56 @@
 # Examples and attemps
 
 library(ggplot2)
+
 devtools::load_all() # load jitter2d functions
+
+library(ggplot2)
+data(mpg)
+
+p <- mpg |> ggplot(aes(x=cty, y=hwy))
+
+p  + geom_point()
+
+p + geom_jitter2D(weight = .05)
+
+p2 <- p + geom_jitter2D() +
+  theme(aspect.ratio = 1) +
+  labs(title = 'gaussian')
+
+p1 <- p + geom_jitter( ) + theme(aspect.ratio = 1) +
+  labs(title = 'jitter')
+
+#===================================================
+
+
+data <- mpg[, c('cty', 'hwy')]
+
+# Load required packages
+library(randtoolbox)
+
+# Generate the Sobol sequence (uniform in [0,1])
+sobol_seq <- sobol(n = nrow(data), dim = 2)
+
+# Transform uniform to standard normal using inverse normal CDF
+normal_seq <- qnorm(sobol_seq)
+
+# Define parameters for bivariate Gaussian
+vv <- data |> as.matrix() |> stats::var(na.rm = TRUE)
+
+# Transform to desired bivariate Gaussian distribution
+# Using Cholesky decomposition
+L <- chol(vv)
+gaussian_seq <- t(L %*% t(normal_seq)) + rep(c(0, 0), each = nrow(data) )
+
+library(dplyr)
+p3 <- mpg |>
+  mutate( cty= cty+gaussian_seq[,1], hwy=hwy+gaussian_seq[,2]) |>
+  ggplot(aes(x=cty, y=hwy) ) + geom_point() +
+  theme(aspect.ratio = 1) +
+  labs(title = 'quasi')
+
+
+library(patchwork)
+p1 + p2 + p3
+
 
