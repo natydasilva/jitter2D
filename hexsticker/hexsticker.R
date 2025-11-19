b0572d2749c103d2d162217024268f188dc6f968
JayaniLakshika <jayanilakshika76@gmail.com>
2025-11-20 01:56:04 +1100

    Need to fix the bug with hexsticker generation

--- hexsticker/hexsticker.R
+++ hexsticker/hexsticker.R
@@ -6,10 +6,10 @@ sticker(
   subplot = here::here("hexsticker/jitter2D.jpeg"),  # path to your image
   package = "jitter2D",
   p_size = 18,
-  p_color = "grey50",
+  p_color = "#d95f02",
   s_x = 1, s_y = 0.8,      # adjust position
-  s_width = 0.6, s_height = 0.6, # scale
+  s_width = 0.73, s_height = 0.73, # scale
   h_fill = "#FFFFFF",
-  h_color = "grey50",
+  h_color = "#d95f02",
   filename = here::here("man/figures/logo.png")
 )
